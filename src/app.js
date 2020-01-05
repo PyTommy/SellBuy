8162f8fc69c8ae85e9f101dad6048144a9723e08
Your Name <you@example.com>
2020-01-06 01:50:23 +0900

    Prepared for deployment

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require('./db/mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const { handleError } = require('./middleware/error');
+const path = require('path');
 
 // Init app
 const app = express();
@@ -19,8 +20,17 @@ app.use('/api/user', require('./routers/user'));
 app.use('/api/products', require('./routers/products'));
 
 app.use((err, req, res, next) => {
-    handleError(err, res);
+    if (err) handleError(err, res);
+    next()
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // Listening
 const port = process.env.PORT;
