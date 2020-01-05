290392a055179f7b0a6ff3e72b002e74882d16e9
Your Name <you@example.com>
2020-01-06 02:18:14 +0900

    GET '*' to GET '/*' for serving html

--- src/middleware/uploadAvatar.js
+++ src/middleware/uploadAvatar.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const uploadAvatar = multer({
         limits: {
-            fileSize: 1000000
+            fileSize: 3000000
         },
         fileFilter( req, file, cb) {
             console.log(file);
